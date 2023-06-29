e2c90349284afafa50efec75df37f5bc3507942e
Segura Mendoza Gael Alejandro <108765188+GaelSM@users.noreply.github.com>
2023-06-29 15:16:15 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ export default function App() {
 
         const res = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_hv3UPa3n7TWDomO3dtQd8ACogGBBp&${type}=${information}`)
         
-        if(!res.ok){
+        if(!res.ok || res.code === 400){
           formRef.current.classList.add("error")
           return
         }
